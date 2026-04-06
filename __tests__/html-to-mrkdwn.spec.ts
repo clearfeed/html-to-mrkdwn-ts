cc13724a58cd6f9c1774d4c52e0855841dbecc8b
Pushpendra Chandravanshi <pushpendrachandravanshi4@gmail.com>
2026-04-06 21:37:23 +0530

    remove test

--- __tests__/html-to-mrkdwn.spec.ts
+++ __tests__/html-to-mrkdwn.spec.ts
@@ -18,21 +18,4 @@ describe('Transform HTML to Slack mrdkwn', () => {
       text: '*Bold Text*'
     })
   })
-
-  it('When HTML text contains an unordered list, bullet points are indented', () => {
-    const html = `<p>test</p><ul><li>first</li><li>second</li><li>third</li></ul>`
-    const actual = htmlToMrkdwn(html)
-    expect(actual).toEqual({
-      image: '',
-      text: 'test\n   • first\n   • second\n   • third'
-    })
-  })
-
-  it('When HTML text contains a nested unordered list, nested bullets have deeper indentation', () => {
-    const html = `<ul><li>parent<ul><li>child</li></ul></li></ul>`
-    const actual = htmlToMrkdwn(html)
-    // top-level: 3 spaces, nested: 6 spaces
-    expect(actual.text).toContain('   • parent')
-    expect(actual.text).toContain('      • child')
-  })
 })
